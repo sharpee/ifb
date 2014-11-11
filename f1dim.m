7b15b411ab5cf2f4a825f651028af4814bbbb45c
jkaardal <jkaardal@gmail.com>
2014-11-10 16:42:18 -0800

    Update f1dim.m

--- f1dim.m
+++ f1dim.m
@@ -1,9 +1,10 @@
 % Move pcom (x units in xicom direction), and then evaluate the function there.
 
 function [f] = f1dim(x, F, stimulus, P)
-% Must accompany minfunc.
+% Based on f1dim.c from Numerical Recipes in C (1992)
+% by W.H. Press, S.A. Teukolsky, W.T. Vetterling, and B.P. Flannery 
 
-global pcom; % Defned in minfunc.
+global pcom; % Defined in minfunc.
 global xicom;
 global nrfunc;
 
